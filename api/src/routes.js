925568052a94f00040cb61f07dd928784897867f
LucasAlesterio <lucasalesteriomarquesvieira@gmail.com>
2020-10-30 17:50:36 -0300

    reset api

--- api/src/routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-//const loginController = require('./controllers/login');
-//const fileController = require('./controllers/file');
-const routes = express.Router();
-
-//routes.post('/createuser',loginController.create);
-
-module.exports = routes;
