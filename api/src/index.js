ee2ef91efaebdb26f82974d3b2cbcde70846dff2
LucasAlesterio <lucasalesteriomarquesvieira@gmail.com>
2020-10-30 18:26:02 -0300

    api

---
+++ api/src/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const routes = require('./routes');
+const cors = require('cors');
+const app = express();
+
+app.use(cors(
+    //origin:'link'
+));
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
---
+++ api/src/routes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+//const loginController = require('./controllers/login');
+
+const routes = express.Router();
+
+//routes.post('/createuser',loginController.create);
+
+module.exports = routes;
