9adc855299210b0f36c11a68b28cd70fa1ec76c5
LucasAlesterio <lucasalesteriomarquesvieira@gmail.com>
2020-11-18 13:54:04 -0300

    Correção time line

--- api/src/controller/linkController.js
+++ api/src/controller/linkController.js
@@ -304,7 +304,7 @@ module.exports = {
         count = await Link.find().select('name').where('user').in(_user.friends).countDocuments();
         //console.log(!((count-pageSize)<0));
         let sk = 0;
-        let lim = count - pageSize;
+        let lim = count - (pageSize * page);
         if(((count-pageSize)>0) && ((page + 1)*pageSize)<count){
             sk = ((count)-(pageSize*(page + 1)));
             lim = pageSize;
