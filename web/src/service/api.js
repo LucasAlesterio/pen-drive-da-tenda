5f1bb28ae516afcd56236c3df7b63e1f62c15c30
LucasAlesterio <lucasalesteriomarquesvieira@gmail.com>
2020-12-12 15:48:35 -0300

    route

--- web/src/service/api.js
+++ web/src/service/api.js
@@ -1,7 +1,7 @@
 import axios from  'axios';
 
 const api = axios.create({
-    //baseURL:'https://trabalho-final-twm.rj.r.appspot.com'
-    baseURL:'http://localhost:8080'
+    baseURL:'https://trabalho-final-twm.rj.r.appspot.com'
+    //baseURL:'http://localhost:8080'
 })
 export default api;
